431c8170c667fb029994ac4844860a12aaf756b7
georgipetkov18 <george18petkov@gmail.com>
2022-02-13 15:46:48 +0200

    Implement Authentication Controller

---
+++ OnlineSchoolApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineSchoolApi.Controllers
+{
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Authenticate()
+        {
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+    }
+}
