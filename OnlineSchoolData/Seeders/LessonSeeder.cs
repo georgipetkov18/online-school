179a6469d72465a924837eeaaa334537890f5909
georgipetkov18 <george18petkov@gmail.com>
2022-02-27 15:38:57 +0200

    Add LessonSeeder

---
+++ OnlineSchoolData/Seeders/LessonSeeder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineSchoolData.Entities;
+
+namespace OnlineSchoolData.Seeders
+{
+    public static class LessonSeeder
+    {
+        public static async Task SeedLessonAsync(ApplicationDbContext context)
+        {
+            await context.Database.EnsureCreatedAsync();
+            if (!await context.Lessons.AnyAsync())
+            {
+                await context.Lessons.AddAsync(new LessonEntity
+                {
+                    From = new TimeSpan(10, 0, 0),
+                    DurationInMinutes = 40,
+                }); ;
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
