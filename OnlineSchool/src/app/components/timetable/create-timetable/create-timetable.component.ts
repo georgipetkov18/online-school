09b400f4c326dbf1aec2573b3ae813f86314dcb4
Georgi Petkov <george18petkov@gmail.com>
2022-03-27 17:27:52 +0300

    Add modal for creating timetable entry

--- OnlineSchool/src/app/components/timetable/create-timetable/create-timetable.component.ts
+++ OnlineSchool/src/app/components/timetable/create-timetable/create-timetable.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { TimetableEntryRequest } from 'src/app/models/request/timetable-entry-request.model';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+
+import { SubjectsService } from 'src/app/services/subjects.service';
+import { SubjectResponse } from 'src/app/models/response/subject-response.model';
 
 @Component({
   selector: 'app-create-timetable',
@@ -9,13 +14,20 @@ import { TimetableEntryRequest } from 'src/app/models/request/timetable-entry-re
 export class CreateTimetableComponent implements OnInit {
   public daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
   public lessonsCount = 7;
+  public subjectResponses: SubjectResponse[] = [];
+  public suggestions: string[] = [];
   public lessonsArray: number[] = [];
   public timetable: (TimetableEntryRequest | null)[][] = [];
+  private modalRef!: NgbModalRef;
+  private currentRow = -1;
+  private currentCol = -1;
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+    private subjectsService: SubjectsService) { }
 
   ngOnInit(): void {
-    this.lessonsArray = Array(this.lessonsCount).fill(0).map((_, i) => i + 1);
+    this.lessonsArray = Array(this.lessonsCount).fill(0).map((_, i) => i);
     this.timetable = Array(this.lessonsCount).fill(null).map(_ =>
       Array(this.daysOfWeek.length).fill(null)
     );
@@ -25,6 +37,43 @@ export class CreateTimetableComponent implements OnInit {
     const nextNumber = this.lessonsArray[this.lessonsArray.length - 1] + 1;
     this.lessonsArray.push(nextNumber);
     this.updateTable();
+  }
+
+  openModal(content: any, row: number, col: number) {
+    this.currentRow = row;
+    this.currentCol = col;
+    this.modalRef = this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  onChangeSearch(source: 'subjects' | 'lessons' | 'teachers', search: string) {
+    switch (source) {
+      case 'subjects':
+        if (search.length < 3) {
+          this.subjectResponses = [];
+          this.suggestions = [];
+          return;
+        }
+        this.subjectsService.getAllSubjects(search).subscribe({
+          next: subjects => {
+            this.subjectResponses = subjects;
+            this.suggestions = subjects.map(s => s.name);
+          }
+        })
+        break;
+
+      case 'lessons':
+        break;
+
+      case 'teachers':
+        break;
+    }
+  }
+
+  onSubmitModal(modalForm: NgForm) {
+    // Get data from the form
+    // Call the creaty timetable entry endpoint
+    // Save the returned output into this.timetable[this.currentRow][this.currentCol]
+    // Close modal
   }
 
   private updateTable() {
