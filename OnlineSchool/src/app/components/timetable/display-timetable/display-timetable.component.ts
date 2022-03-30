869ae252e7560d3b8b70536025032e997ed8187e
georgipetkov18 <george18petkov@gmail.com>
2022-03-30 11:18:50 +0300

    Remove unnecessary variables

--- OnlineSchool/src/app/components/timetable/display-timetable/display-timetable.component.ts
+++ OnlineSchool/src/app/components/timetable/display-timetable/display-timetable.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FullTimetable } from 'src/app/models/full-timetable.model';
 import { TimetableEntryResponse } from 'src/app/models/response/timetable-entry-response.model';
 import { TimetableService } from 'src/app/services/timetable.service';
-import { DayOfWeek } from '../create-timetable/create-timetable.component';
 
 @Component({
   selector: 'app-display-timetable',
@@ -10,7 +8,6 @@ import { DayOfWeek } from '../create-timetable/create-timetable.component';
   styleUrls: ['./display-timetable.component.css']
 })
 export class DisplayTimetableComponent implements OnInit {
-  public daysOfWeek: DayOfWeek[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
   public data: TimetableEntryResponse[][] = [];
 
   constructor(private timetableService: TimetableService) { }
