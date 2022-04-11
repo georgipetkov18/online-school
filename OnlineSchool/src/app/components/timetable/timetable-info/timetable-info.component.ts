61c8825975a5c680d4d7752b8609c1423e704a15
georgipetkov18 <george18petkov@gmail.com>
2022-04-11 12:53:07 +0300

    Basic responsive design for main page

--- OnlineSchool/src/app/components/timetable/timetable-info/timetable-info.component.ts
+++ OnlineSchool/src/app/components/timetable/timetable-info/timetable-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TeacherResponse } from 'src/app/models/response/teacher-response.model';
+import { TimetableEntryResponse } from 'src/app/models/response/timetable-entry-response.model';
 import { SignalRService } from 'src/app/services/signal-r.service';
 
 @Component({
@@ -7,6 +9,24 @@ import { SignalRService } from 'src/app/services/signal-r.service';
   styleUrls: ['./timetable-info.component.css']
 })
 export class TimetableInfoComponent implements OnInit, OnDestroy {
+
+  public current: TimetableEntryResponse = {
+    name: 'test current',
+    code: 'my test current code',
+    teacher: new TeacherResponse('4hy7tgurebv27', 'Teacher', 'Current'),
+    class: '12e',
+    from: '12:00:00',
+    to: '13:00:00'
+  };
+
+  public next: TimetableEntryResponse = {
+    name: 'test next',
+    code: 'my test next code',
+    teacher: new TeacherResponse('4hy7tgurebv27', 'Teacher', 'Next'),
+    class: '12e',
+    from: '16:00:00',
+    to: '20:00:00'
+  };
 
   constructor(private signalRService: SignalRService) { }
 
