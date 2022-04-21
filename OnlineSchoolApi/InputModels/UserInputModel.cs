b5c54dadafe15ad5222c016a4f827e4d67c35726
georgipetkov18 <george18petkov@gmail.com>
2022-04-21 16:11:38 +0300

    Add some validations to the backend

--- OnlineSchoolApi/InputModels/UserInputModel.cs
+++ OnlineSchoolApi/InputModels/UserInputModel.cs
@@ -1,25 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BusinessLayer.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineSchoolApi.InputModels
 {
     public class UserInputModel
     {
         [Required]
+        [StringLength(128, MinimumLength = 3)]
         public string Username { get; set; } = null!;
 
         [Required]
+        [StringLength(65, MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,65}$")]
         public string Password { get; set; } = null!;
 
         [Required]
+        [MaxLength(128)]
         public string FirstName { get; set; } = null!;
 
         [Required]
+        [MaxLength(128)]
         public string LastName { get; set; } = null!;
 
+        [Email]
         [Required]
-        [EmailAddress]
         public string Email { get; set; } = null!;
 
+        [Required]
         public string RoleName { get; set; } = null!;
 
         public Guid? ClassId { get; set; }
---
+++ OnlineSchoolBusinessLogic/Common/EmailAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BusinessLayer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EmailAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            return Regex.IsMatch(value!.ToString()!,
+                "^(([^<>()[\\]\\\\.,;:\\s@\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
+        }
+    }    
+}
